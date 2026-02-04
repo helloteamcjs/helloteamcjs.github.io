f69d5efa40fe711650f9ad0fd2936d441eaac75b
helloteamcjs <helloteamcjs@gmail.com>
2026-02-04 22:04:52 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -108,9 +108,6 @@ CICA 미술관, 김포.
         { src: "images/drawing/t-2.webp", size: 1.5 },
         { src: "images/drawing/t-1.webp", size: 1.5 },
         { src: "images/drawing/c-4.webp", size: 1.5 },
-        { src: "images/drawing/c-2.webp" },
-        { src: "images/drawing/c-1.webp" },
-        { src: "images/drawing/c-3.webp" },
         { src: "images/drawing/c-6.webp", break: true },
         { src: "images/drawing/c-7.webp" },
         { src: "images/drawing/c-5.webp", size: 1.5 }
